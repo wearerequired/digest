2688f393604463b03b052f18c0921b6bce23a6f7
Pascal Birchler <hello@pascalbirchler.ch>
2017-02-19 17:10:27 +0100

    Fix company name

--- js/src/digest.js
+++ js/src/digest.js
@@ -1,16 +1,16 @@
 /**
  * Digest Notifications
  *
- * Copyright (c) 2015 required+
+ * Copyright (c) 2015-2017 required
  * Licensed under the GPLv2+ license.
  */
 
-(function () {
-	var frequency_period = document.getElementById('digest_frequency_period'),
-			frequency_day_wrapper = document.getElementById('digest_frequency_day_wrapper');
+(function() {
+	var frequency_period      = document.getElementById( 'digest_frequency_period' ),
+	    frequency_day_wrapper = document.getElementById( 'digest_frequency_day_wrapper' );
 
 	function hideAndSeek() {
-		if ('weekly' === ( this.value || frequency_period.value )) {
+		if ( 'weekly' === ( this.value || frequency_period.value ) ) {
 			frequency_day_wrapper.className = '';
 		} else {
 			frequency_day_wrapper.className = 'digest-hidden';
