b92266930a148e6b807d2c40d4cc6f0aea029d50
Pascal Birchler <hello@pascalbirchler.ch>
2017-02-19 21:11:53 +0100

    Use `assertEqualSetsWithDelta()`

--- tests/phpunit/tests/Plugin.php
+++ tests/phpunit/tests/Plugin.php
@@ -35,24 +35,4 @@ class Plugin extends WP_UnitTestCase {
 
 		$this->assertSame( 0, $action->get_call_count() );
 	}
-
-	public function test_comment_moderation_recipients() {
-		digest()->comment_moderation_recipients( array(
-			'foo@example.com',
-			'bar@example.com',
-		), 123 );
-
-		$queue = Queue::get();
-
-		$this->assertArrayHasKey( 'foo@example.com', $queue );
-		$this->assertArrayHasKey( 'bar@example.com', $queue );
-		$this->assertCount( 1, $queue['foo@example.com'] );
-		$this->assertCount( 1, $queue['bar@example.com'] );
-		$this->assertSame( 'comment_moderation', $queue['foo@example.com'][0][1] );
-		$this->assertSame( 'comment_moderation', $queue['bar@example.com'][0][1] );
-		$this->assertSame( 123, $queue['foo@example.com'][0][2] );
-		$this->assertSame( 123, $queue['bar@example.com'][0][2] );
-
-		Queue::clear();
-	}
 }
--- tests/phpunit/tests/Queue.php
+++ tests/phpunit/tests/Queue.php
@@ -6,72 +6,88 @@ use \WP_UnitTestCase;
 use \Required\Digest\Queue as Digest_Queue;
 
 class Queue extends WP_UnitTestCase {
+	public function setUp() {
+		parent::setUp();
+
+		Digest_Queue::clear();
+	}
+
+	public function tearDown() {
+		Digest_Queue::clear();
+
+		parent::tearDown();
+	}
+
+	public function assertEqualSetsWithDelta($expected, $actual) {
+		sort( $expected );
+		sort( $actual );
+		$this->assertEquals( $expected, $actual, '', 1 );
+	}
+
 	public function test_empty_queue() {
 		$this->assertEmpty( Digest_Queue::get() );
 	}
 
 	public function test_queue_clearing() {
 		Digest_Queue::add( 'johndoe@example.org', 'foo', 'bar' );
-		$this->assertEquals( 1, count( Digest_Queue::get() ) );
+
+		$queue_1 = Digest_Queue::get();
 		Digest_Queue::clear();
-		$this->assertEquals( 0, count( Digest_Queue::get() ) );
+		$queue_2 = Digest_Queue::get();
+
+		$this->assertEquals( 1, count( $queue_1 ) );
+		$this->assertEquals( 0, count( $queue_2 ) );
 	}
 
 	public function test_queue_add() {
-		$current_time = current_time( 'timestamp' );
-
 		$expected = array(
-			'johndoe@example.org' => array(
+			'foo@example.com' => array(
 				array(
-					$current_time,
+					current_time( 'timestamp' ),
 					'foo',
 					'bar',
 				),
 			),
 		);
 
-		Digest_Queue::add( 'johndoe@example.org', 'foo', 'bar' );
+		Digest_Queue::add( 'foo@example.com', 'foo', 'bar' );
 
-		$this->assertEquals( $expected, Digest_Queue::get() );
+		$this->assertEqualSetsWithDelta( $expected, Digest_Queue::get() );
 	}
 
 	public function test_queue_add_duplicate() {
-		$current_time = current_time( 'timestamp' );
-
 		$expected = array(
-			'johndoe@example.org' => array(
+			'foo@example.com' => array(
 				array(
-					$current_time,
+					current_time( 'timestamp'),
 					'foo',
 					'bar',
 				),
 				array(
-					$current_time,
+					current_time( 'timestamp'),
 					'hello',
 					'world',
 				),
 			),
 		);
 
-		Digest_Queue::add( 'johndoe@example.org', 'foo', 'bar' );
-		Digest_Queue::add( 'johndoe@example.org', 'hello', 'world' );
+		Digest_Queue::add( 'foo@example.com', 'foo', 'bar' );
+		Digest_Queue::add( 'foo@example.com', 'hello', 'world' );
 
-		$this->assertEquals( $expected, Digest_Queue::get() );
+		$this->assertEqualSetsWithDelta( $expected, Digest_Queue::get() );
 	}
 
 	/**
 	 * Test adding many entries to the queue.
 	 *
-	 * @dataProvider get_queue_entries
+	 * @dataProvider data_queue_entries
 	 *
 	 * @param string $recipient The recipient.
 	 * @param string $event     The type of the event.
 	 * @param string $data      Data to store for this event, for example a comment ID.
 	 */
 	public function test_queue_add_many( $recipient, $event, $data ) {
-		$current_time = current_time( 'timestamp' );
-
-		$expected = array( array( $current_time, $event, $data ) );
+		$expected = array( array( current_time( 'timestamp' ), $event, $data ) );
 
 		Digest_Queue::add( $recipient, $event, $data );
 
@@ -79,15 +95,41 @@ class Queue extends WP_UnitTestCase {
 
 		$this->assertArrayHasKey( $recipient, $queue );
 		$this->assertEquals( 1, count( $queue[ $recipient ] ) );
-		$this->assertEquals( $expected, $queue[ $recipient ] );
+		$this->assertEqualSetsWithDelta( $expected, $queue[ $recipient ] );
 	}
 
-	public function get_queue_entries() {
+	public function data_queue_entries() {
 		return array(
 			array( 'johndoe@example.org', 'foo', 'bar' ),
 			array( 'janedoe@example.org', '', '' ),
 			array( 'foo@example.org', 0, 0 ),
 			array( 'bar@example.org', 'foo', 'bar' ),
 		);
+	}
+
+	public function test_comment_moderation_recipients() {
+		digest()->comment_moderation_recipients( array(
+			'foo@example.com',
+			'bar@example.com',
+		), 123 );
+
+		$expected = array(
+			'foo@example.com' => array(
+				array(
+					current_time( 'timestamp'),
+					'comment_moderation',
+					123,
+				),
+			),
+			'bar@example.com' => array(
+				array(
+					current_time( 'timestamp'),
+					'comment_moderation',
+					123,
+				),
+			),
+		);
+
+		$this->assertEqualSetsWithDelta( $expected, Digest_Queue::get() );
 	}
 }
