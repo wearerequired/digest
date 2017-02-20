bfd354f5ecb816b510d64c67e8252b50c89f9b95
Pascal Birchler <hello@pascalbirchler.ch>
2017-02-20 13:18:36 +0100

    More email tests

--- tests/phpunit/tests/Plugin.php
+++ tests/phpunit/tests/Plugin.php
@@ -2,6 +2,7 @@
 
 namespace Required\Digest\Tests;
 
+use MockPHPMailer;
 use Required\Digest\Queue;
 use \WP_UnitTestCase;
 use \MockAction;
@@ -34,5 +35,28 @@ class Plugin extends WP_UnitTestCase {
 		remove_filter( 'digest_cron_email_message', array( $action, 'filter' ) );
 
 		$this->assertSame( 0, $action->get_call_count() );
+	}
+
+	public function test_send_email_non_empty_queue() {
+		Queue::add( 'foo@example.com', 'foo', 'bar' );
+
+		digest()->send_email( 'Foo' );
+
+		/** @var MockPHPMailer $mailer */
+		$mailer = tests_retrieve_phpmailer_instance();
+
+		$this->assertSame( 'Foo', $mailer->Subject );
+	}
+
+	public function test_send_email_non_empty_queue_registered_events() {
+		Queue::add( 'foo@example.com', 'core_update_success', '100.1.0' );
+
+		digest()->send_email( 'Foo' );
+
+		/** @var MockPHPMailer $mailer */
+		$mailer = tests_retrieve_phpmailer_instance();
+
+		$this->assertSame( 'Foo', $mailer->get_sent()->subject );
+		$this->assertContains( 'Hi there', $mailer->get_sent()->body );
 	}
 }
