820c8340c495e6abfbaf4c1dc6a316a2618fee04
Dominik Schilling <dominikschilling+git@gmail.com>
2021-05-26 22:47:36 +0200

    Fix build tooling
    * Use strict versions
    * Move all dependencies to devDependenciesAdd missing dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,17 @@
 const path = require( 'path' );
 const TerserPlugin = require( 'terser-webpack-plugin' );
 
-const externals = {};
-
 const isProduction = process.env.NODE_ENV === 'production';
 
-const baseConfig = {
+module.exports = {
 	mode: isProduction ? 'production' : 'development',
 
-	devtool: isProduction ? undefined : 'inline-source-map',
+	devtool: isProduction ? undefined : 'eval-source-map',
 
 	stats: 'errors-only',
 
 	// https://webpack.js.org/configuration/optimization/#optimization-runtimechunk
 	optimization: {
-		runtimeChunk: false,
 		minimizer: [
 			new TerserPlugin( {
 				parallel: true,
@@ -34,34 +31,25 @@ const baseConfig = {
 	// https://webpack.js.org/configuration/entry-context/#context
 	context: path.resolve( __dirname, 'js/src' ),
 
-	// https://webpack.js.org/configuration/externals/
-	externals,
-};
-
-module.exports = [
-	{
-		...baseConfig,
-
-		// https://webpack.js.org/configuration/entry-context/#entry
-		entry: {
-			digest: './digest.js',
-		},
-
-		// https://webpack.js.org/configuration/output/
-		output: {
-			path: path.resolve( __dirname, 'js' ),
-			filename: '[name].js',
-		},
-
-		// https://github.com/babel/babel-loader#usage
-		module: {
-			rules: [
-				{
-					test: /\.js$/,
-					exclude: /node_modules/,
-					use: 'babel-loader',
-				},
-			],
-		},
+	// https://webpack.js.org/configuration/entry-context/#entry
+	entry: {
+		digest: './digest.js',
 	},
-];
+
+	// https://webpack.js.org/configuration/output/
+	output: {
+		path: path.resolve( __dirname, 'js' ),
+		filename: '[name].js',
+	},
+
+	// https://github.com/babel/babel-loader#usage
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: 'babel-loader',
+			},
+		],
+	},
+};
