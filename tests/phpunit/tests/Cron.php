e74485d9124e807a04e7c4341f532c789d8f1cb9
Dominik Schilling <dominikschilling+git@gmail.com>
2023-03-27 16:15:58 +0200

    Add monthly notifications (#28)
    * Add monthly notifications
    
    * Limit wp_date filter to only the day formats
    
    * Add another test

--- tests/phpunit/tests/Cron.php
+++ tests/phpunit/tests/Cron.php
@@ -68,6 +68,61 @@ class Cron extends WP_UnitTestCase {
 		);
 	}
 
+	public function test_run_cron_monthly() {
+		update_option( 'digest_frequency', [
+			'period' => 'monthly',
+			'hour'   => date( 'G' ),
+		] );
+
+		Queue::add( 'foo@example.com', 'foo', 'bar' );
+
+		add_filter( 'wp_date', [ $this, 'filter_wp_date_to_return_first_day_of_month' ], 10, 2 );
+		add_filter( 'digest_cron_email_subject', [ $this, 'filter_digest_cron_email_subject' ] );
+		Digest_Cron::init();
+		remove_filter( 'digest_cron_email_subject', [ $this, 'filter_digest_cron_email_subject' ] );
+		remove_filter( 'wp_date', [ $this, 'filter_wp_date_to_return_first_day_of_month' ] );
+
+		$this->assertSame(
+			sprintf( __( 'Past Month on %s', 'digest' ), get_bloginfo( 'name' ) ),
+			$this->email_subject
+		);
+	}
+
+	public function filter_wp_date_to_return_first_day_of_month( $date, $format ) {
+		if ( 'Y-m-d' !== $format || 'Y-m-01' === $format ) {
+			return $date;
+		}
+
+		return gmdate( 'Y-m-01' );
+	}
+
+	public function test_run_cron_monthly_not_first_day_of_month() {
+		update_option( 'digest_frequency', [
+			'period' => 'monthly',
+			'hour'   => date( 'G' ),
+		] );
+
+		Queue::add( 'foo@example.com', 'foo', 'bar' );
+
+		$action = new MockAction();
+
+		add_filter( 'wp_date', [ $this, 'filter_wp_date_to_return_second_day_of_month' ], 10, 2 );
+		add_filter( 'digest_cron_email_subject', [ $action, 'filter' ] );
+		Digest_Cron::init();
+		remove_filter( 'digest_cron_email_subject', [ $action, 'filter' ] );
+		remove_filter( 'wp_date', [ $this, 'filter_wp_date_to_return_second_day_of_month' ] );
+
+		$this->assertSame( 0, $action->get_call_count() );
+	}
+
+	public function filter_wp_date_to_return_second_day_of_month( $date, $format ) {
+		if ( 'Y-m-d' !== $format ) {
+			return $date;
+		}
+
+		return gmdate( 'Y-m-02' );
+	}
+
 	public function test_run_cron_empty_queue() {
 		update_option( 'digest_frequency', [
 			'period' => 'daily',
